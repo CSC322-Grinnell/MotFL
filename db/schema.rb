cf47ec0ec5e8a0f8371ee7df506da5b605628d08
Agne Lukoseviciute <lukosevi@grinnell.edu>
2019-03-11 15:27:06 +0000

    Adding password encryption

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190224192656) do
+ActiveRecord::Schema.define(version: 20190307174312) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190224192656) do
     t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,23 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  
+  #maybe add last_name later 
+  def setup
+    @user = User.new(first_name: "Example", last_name: "User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  
+    test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 8
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 7
+    assert_not @user.valid?
+  end
+
+  
 end
