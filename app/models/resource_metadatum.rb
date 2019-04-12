8e4acaef0bb47c9ce6ea4806d04983743145698b
Henry <henry.mahar@gmail.com>
2019-04-12 10:57:37 -0500

    Added search and fixed resource tag naming convention
    rake db will need to be run for the migration

--- app/models/resource_metadatum.rb
+++ app/models/resource_metadatum.rb
@@ -4,7 +4,10 @@ class ResourceMetadatum < ApplicationRecord
 
 	def self.search(search)
 		if search
-			resources = ResourceMetadatum.joins(:tags).find_by(Tag_Title: search)
+			resources = ResourceMetadatum.joins(:tags).where('Tag_Title LIKE :search OR
+				Author LIKE :search OR 
+			   Title LIKE :search OR
+			   Abstract LIKE :search', search: "%#{search.downcase}%")
 			if resources
 				resources
 			end
