f0cfff51ece93b99d937b15e5de544edd1e4d106
Henry <henry.mahar@gmail.com>
2019-04-26 10:30:04 -0500

    fixed author search misnamed field

--- app/models/resource_metadatum.rb
+++ app/models/resource_metadatum.rb
@@ -7,7 +7,7 @@ class ResourceMetadatum < ApplicationRecord
 	def self.search(search)
 		if search
 			resources = ResourceMetadatum.joins(:tags).joins(:authors).where('Tag_Title LIKE :search OR
-				Author LIKE :search OR 
+				author_name LIKE :search OR 
 			   Title LIKE :search OR
 			   Abstract LIKE :search', search: "%#{search.downcase}%").distinct
 			if resources
