2ad7b93faf5f60f9609c102779db63a3c8aa9b1c
Agne Lukoseviciute <lukosevi@grinnell.edu>
2019-05-01 15:48:54 +0000

    fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
+      #log_in -> login_url
+      login_url user
       redirect_to user
     else
       # Create an error message.
