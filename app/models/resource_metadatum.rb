308bd53c84037611be82fb0081ad88e5ac981967
Henry <henry.mahar@gmail.com>
2019-05-14 10:49:27 -0500

    Fixed resource edit and display, updated schema

--- app/models/resource_metadatum.rb
+++ app/models/resource_metadatum.rb
@@ -5,7 +5,7 @@ class ResourceMetadatum < ApplicationRecord
 	has_many :authors, :through => :resource_metadata_authors
 
 	def self.search(search)
-		if search != ''
+		if search != nil
 			sql = "SELECT resource_metadata.id, Title, GROUP_CONCAT(authors.author_name, ', ') as authors, publish_date, Abstract, Link 
 					FROM resource_metadata 
 					LEFT JOIN resource_metadata_authors ON resource_metadata_authors.resource_metadatum_id = resource_metadata.id 
@@ -13,11 +13,11 @@ class ResourceMetadatum < ApplicationRecord
 					LEFT JOIN resource_tags ON resource_tags.resource_metadatum_id = resource_metadata.id
 					LEFT JOIN tags ON tags.id = resource_tags.tag_id
 					GROUP BY resource_metadata.id
-					HAVING Tag_Title LIKE '%#{search}%' OR
-			   				Title LIKE '%#{search}%' OR
-			   				Abstract LIKE '%#{search}%'OR
-								authors LIKE '%#{search}%' OR
-								Link LIKE '%#{search}%'"
+					HAVING Tag_Title LIKE '%#{search.downcase}%' OR
+			   				Title LIKE '%#{search.downcase}%' OR
+			   				Abstract LIKE '%#{search.downcase}%'OR
+								authors LIKE '%#{search.downcase}%' OR
+								Link LIKE '%#{search.downcase}%'"
 			resources_hash_data = ActiveRecord::Base.connection.execute(sql)
 		else
 			sql = "SELECT resource_metadata.id, Title, GROUP_CONCAT(authors.author_name, ', ') as authors, publish_date, Abstract, Link 
